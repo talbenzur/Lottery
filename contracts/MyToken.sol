0ad4b8955dee65575b50abe57a5b153dfbb10c07
Tal Ben-Zur <76743219+talbenzur@users.noreply.github.com>
2022-07-16 12:42:52 +0300

    Add files via upload

---
+++ contracts/MyToken.sol
@@ -0,0 +1,21 @@
+//pragma solidity ^0.8.11;
+
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+//contract MyToken is ERC20 {
+//address public admin;
+///
+//    constructor() ERC20("MyToken", "TTC") {
+//        _mint(msg.sender, 10000 * 10**18);
+ //       admin = msg.sender;
+//    }
+
+ //   function mint(address to, uint256 amount) external {
+ //       require(msg.sender == admin, "only admin");
+//        _mint(to, amount);
+//    }
+
+  //  function burn(uint256 amount) external {
+ //       _burn(msg.sender, amount);
+//    }
+//}
